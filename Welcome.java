eb9bfb53cec558d5589f81dc18a734d740da67e3
NguyenTranTrongHuy2912 <huy.23671451@student.iuh.edu.vn>
2024-08-16 21:37:40 +0700

    init project

---
+++ Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Nguyen Tran Trong Huy!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
